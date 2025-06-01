d498e75db84958a23b2c2df541fa2591bc291923
dj221ai <djha5051@gmail.com>
2025-06-01 21:24:50 +0530

    fixed mobile view and added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
 import { NavLink, Link } from "react-router"
+import { useState } from "react"
+import ignite from '../assets/ignite.png'
 
 function Header(){
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div>
             <header className="shadow sticky z-50 top-0">
@@ -8,9 +17,9 @@ function Header(){
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <Link to="/" className="flex items-center">
                             <img
-                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                                src={ignite}
                                 className="mr-3 h-12"
-                                alt="Logo"
+                                alt="IgniteFunds&Investments"
                             />
                         </Link>
                         <div className="flex items-center lg:order-2">
@@ -22,17 +31,42 @@ function Header(){
                                 Register
                             </Link>
                         </div>
+                        {/* Hamburger Button */}
+                        <button
+                            type="button"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            onClick={toggleMenu}
+                            aria-expanded={isMenuOpen}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 17 14"
+                            >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 1h15M1 7h15M1 13h15"
+                            />
+                            </svg>
+                        </button>
                         <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
+                        className={`w-full md:block md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}
+            id="navbar-default"
                     >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
                             <li>
                                 <NavLink
                                 to="/"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={() => setIsMenuOpen(false)}
                                 >
                                     Home
                                 </NavLink>
@@ -43,6 +77,7 @@ function Header(){
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={() => setIsMenuOpen(false)}
                                 >
                                     About
                                 </NavLink>
@@ -53,6 +88,7 @@ function Header(){
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={() => setIsMenuOpen(false)}
                                 >
                                     Contact
                                 </NavLink>
@@ -63,6 +99,7 @@ function Header(){
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={() => setIsMenuOpen(false)}
                                 >
                                     Products
                                 </NavLink>
@@ -73,6 +110,7 @@ function Header(){
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
+                                    onClick={() => setIsMenuOpen(false)}
                                 >
                                     Pricing
                                 </NavLink>
